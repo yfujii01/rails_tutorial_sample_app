d2295c114d7a57c38c730203ef52cb8834b2404b
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-16 12:03:15 +0900

    add User new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  get 'signup', to: 'users#new'
+
   root 'static_pages#home'
   get 'static_pages/home'
   # get 'static_pages/help'
@@ -6,7 +9,4 @@ Rails.application.routes.draw do
   # get 'static_pages/about'
   get '/about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
-
-    # 以下の書き方であればrubymineで補完＆定義移動可能
-    # get 'static_pages/contact'
 end
