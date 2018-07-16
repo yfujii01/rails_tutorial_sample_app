029403144fede3300d9a27d781eba3ce3685dd54
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-16 11:52:27 +0900

    midify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
   root 'static_pages#home'
+  get 'static_pages/home'
+  # get 'static_pages/help'
+  get '/help', to: 'static_pages#help', as: 'helf'
+  # get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
+
+    # 以下の書き方であればrubymineで補完＆定義移動可能
+    # get 'static_pages/contact'
 end
