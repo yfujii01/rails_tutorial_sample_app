c744c8d586417bbf86cb06c79141bc2958c460f8
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-16 13:19:33 +0900

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,6 +11,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", helf_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
   end
 
 end
