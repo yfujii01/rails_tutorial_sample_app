dcfbb65f1ea5bd5fbcc06db34695b0876ec080dc
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-17 17:55:30 +0900

    add test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # ログイン成功
 
-      log_in user
-      remember user
+      log_in @user
+      # remember user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
 
-      redirect_to user
+      redirect_to @user
 
     else
       # ログイン失敗
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -46,7 +46,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', user_path(@user)
   end
 
-  test "login with valid information followed by logout" do
+  test 'login with valid information followed by logout' do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
@@ -54,9 +54,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -65,9 +65,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
 
     follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path,      count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+
+    assert_equal @user.email, assigns(:user).email
+
+    assert_not_empty cookies['remember_token']
+  end
+
+  test 'login without remembering' do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
