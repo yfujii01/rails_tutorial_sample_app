21aabecccd7ce13e94f2b2b1011434b3ed44c3c6
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-17 15:04:10 +0900

    ok login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ログイン成功
+
+      log_in user
+
+      redirect_to user
+
     else
       # ログイン失敗
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # ログインしているユーザー情報を返却する
+  def current_user
+    # User.find(session[:user_id])
+
+    # 高速化
+    # if @current_user.nil?
+    #   @current_user = User.find_by(id: session[:user_id])
+    # else
+    #   @current_user
+    # end
+
+    # 単純化1
+    # @current_user = @current_user || User.find_by(id: session[:user_id])
+
+    # 単純化2
+    @current_user ||= User.find_by(id: session[:user_id])
+
+  end
 end
