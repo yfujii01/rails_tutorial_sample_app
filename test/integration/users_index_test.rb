0a45ae30e5be800e1e596ac774a2b860e7342fac
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-22 19:57:00 +0900

    ユーザー一覧画面に対してのテスト追加

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,7 +4,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
-    @admin     = users(:michael)
+    @admin = users(:michael)
     @non_admin = users(:archer)
   end
 
@@ -21,30 +21,72 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
     # ページネーション確認
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
+
+      # アクティベートされている場合だけ表示する
+      if user.activated?
+        # ユーザーページへのリンク確認
+        assert_select 'a[href=?]', user_path(user), text: user.name
+      else
+        # 非アクティベートユーザーへのリンクが存在しないこと
+        assert_select 'a[href=?]', user_path(user), count: 0
+      end
     end
   end
 
   test "index as admin including pagination and delete links" do
+    # 管理者ユーザーでログイン
     log_in_as(@admin)
+
+    # ユーザー一覧ページへ遷移
     get users_path
+
+    # レイアウト確認
     assert_template 'users/index'
     assert_select 'div.pagination'
+
+    # DBから1ページ分のユーザー情報取得
     first_page_of_users = User.paginate(page: 1)
+
+    # ユーザー数分繰り返し
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+
+      # ユーザーがアクティベートされているか確認
+      if user.activated?
+
+        # ユーザーページへの遷移リンク確認
+        assert_select 'a[href=?]', user_path(user), text: user.name
+
+        # 表示ユーザーがログインしたユーザー自身ではない場合
+        unless user == @admin
+          # 表示ユーザーのdeleteリンク確認
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+
+      else
+
+        # 非アクティベートユーザーへのリンクが存在しないこと
+        assert_select 'a[href=?]', user_path(user), count: 0
+
       end
     end
+
+    # DBから削除されることの確認
     assert_difference 'User.count', -1 do
+
+      # ユーザーを一人deleteリンクから削除
       delete user_path(@non_admin)
     end
   end
 
   test "index as non-admin" do
+
+    # 非管理者ユーザーでログイン
     log_in_as(@non_admin)
+
+    # ユーザー一覧ページへ遷移
     get users_path
+
+    # deleteリンクが存在しないこと
     assert_select 'a', text: 'delete', count: 0
   end
 
