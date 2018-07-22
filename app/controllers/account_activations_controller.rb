289cc25235ec68e8eb6e69f1d388bbd9d05035db
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-22 16:50:03 +0900

    ログイン時アクティベートされているか確認を追加

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,20 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+
+    # 以下の条件をすべて満たす場合にアクティベーションの後ログインする
+    # 引数emailの一致するユーザーがDBに存在する
+    # ユーザーがアクティベーションされていない
+    # ユーザーのトークンがダイジェストと一致する
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,21 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       # ログイン成功
-
-      log_in @user
-      # remember user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-
-      # redirect_to @user
-      redirect_back_or @user
-
+      if @user.activated?
+        # アクティベートされているユーザーの場合
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        # アクティベートされてないユーザーの場合エラーにする
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # ログイン失敗
-
-      # flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      # flash[:danger] = 'Invalid email/password combination' # 消えない
       flash.now[:danger] = 'Invalid email/password combination'
 
       render 'new'
