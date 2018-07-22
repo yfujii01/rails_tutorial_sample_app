c6a08693b5e882e006ca95b56c3060925ae76915
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-22 20:26:17 +0900

    ログインのテストリファクタ(コメント追加)

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,31 +6,42 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test 'the truth' do
+
+    # ログイン画面へ遷移
     get login_path
 
+    # レイアウト確認
     assert_template 'sessions/new'
 
+    # ログインする(エラー)(emailとパスワードを空白)
     post login_path, params: { session: { email: '',
                                           password: '' } }
 
+    # ログインページが表示されていること
     assert_template 'sessions/new'
 
+    # フラッシュが空白ではないこと(エラーメッセージが表示されている)
     assert_not flash.empty?
     assert flash[:danger]
 
+    # rootページへ遷移する
     get root_path
 
+    # フラッシュが表示されていないこと(画面遷移でメッセージが消えることの確認)
     assert flash.empty?
     assert_not flash[:danger]
   end
 
   test 'login with valid information' do
+    # ログインページヘ遷移する
     get login_path
+
+    # ログインする(正常)
     post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
 
+    # ユーザーページへリダイレクトされていること
     assert_redirected_to @user
-    # ユーザーページへリダイレクト
     follow_redirect!
 
     # レイアウト確認
@@ -44,46 +55,99 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     # ユーザーページへのリンクが存在すること
     assert_select 'a[href=?]', user_path(@user)
+
+    # ユーザー一覧ページへのリンクが存在すること
+    assert_select 'a[href=?]', users_path
   end
 
   test 'login with valid information followed by logout' do
+
+    # ログインページヘ遷移
     get login_path
-    post login_path, params: { session: { email:    @user.email,
+
+    # ログインする(正常)
+    post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
+
+    # ログインできていることの確認(sessionに登録できているか)
     assert is_logged_in?
+
+    # ユーザーページへリダイレクトされていることの確認
     assert_redirected_to @user
     follow_redirect!
+
+    # レイアウト確認
     assert_template 'users/show'
+
+    # ログインページヘのリンクが無いこと
     assert_select 'a[href=?]', login_path, count: 0
+
+    # ログアウトリンクがあること
     assert_select 'a[href=?]', logout_path
+
+    # ユーザーページへのリンクがあること
     assert_select 'a[href=?]', user_path(@user)
+
+    # ユーザー一覧ページへのリンクが存在すること
+    assert_select 'a[href=?]', users_path
+
+    # ログアウトする
     delete logout_path
+
+    # ログアウトされていること(sessionが削除されていること)
     assert_not is_logged_in?
+
+    # rootページへ遷移すること
     assert_redirected_to root_url
 
     # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
     delete logout_path
-
     follow_redirect!
+
+    # ログインページヘのリンクがあること
     assert_select 'a[href=?]', login_path
-    assert_select 'a[href=?]', logout_path,      count: 0
+
+    # ログアウトのリンクが無いこと
+    assert_select 'a[href=?]', logout_path, count: 0
+
+    # ユーザーページへのリンクが無いこと
     assert_select 'a[href=?]', user_path(@user), count: 0
+
+    # ユーザー一覧ページへのリンクが無いこと
+    assert_select 'a[href=?]', users_path, count: 0
   end
 
   test 'login with remembering' do
+    # cookieのremember_tokenに値が設定されていないこと
+    assert_equal nil, cookies['remember_token']
+
+    # ログイン状態を記憶してログイン
     log_in_as(@user, remember_me: '1')
 
+    # 直前に作られたインスタンス変数のemailが一致すること
     assert_equal @user.email, assigns(:user).email
 
+    # cookieのremember_tokenに値が設定されていること
     assert_not_empty cookies['remember_token']
   end
 
   test 'login without remembering' do
-    # クッキーを保存してログイン
+    # cookieのremember_tokenに値が設定されていないこと
+    assert_equal nil, cookies['remember_token']
+
+    # ログイン状態を記憶してログイン
     log_in_as(@user, remember_me: '1')
+
+    # ログアウトする
     delete logout_path
-    # クッキーを削除してログイン
+
+    # cookieのremember_tokenに値が設定されていないこと
+    assert_empty cookies['remember_token']
+
+    # ログイン状態を記憶せずにログイン
     log_in_as(@user, remember_me: '0')
+
+    # cookieのremember_tokenに値が設定されていないこと
     assert_empty cookies['remember_token']
   end
 end
