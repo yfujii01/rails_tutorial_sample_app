be1ef37d7dfa90f018f7987af22cd924d170b3a3
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-28 18:20:57 +0900

    13.3.4完了

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -14,6 +15,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+
+    # 元のページに戻る、元のページが取得できない場合はrootへ飛ばす
+    # redirect_to request.referrer || root_url
+    redirect_back(fallback_location: root_url)
+  end
+
+  private
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 
   private
